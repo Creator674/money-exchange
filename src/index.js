3b09767b39af2e05f7019f57c1aaec9a91f9b5ae
Creator674 <666anastyalena@mail.ru>
2019-03-24 10:12:31 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    else if  (currency <= 0 ) {
+        return {};
+};
+var money = {
+  'H' : 0,
+  'Q' : 0,
+  'D' : 0,
+  'N' : 0,
+  'P' : 0,
+};
+count(currency);
+
+for (var key in money) {
+    if (money[key] === 0) {
+        delete money[key];
+    }
 }
+
+return money ;
+
+ function count(currency){
+    if (currency - 50 >= 0) {
+        return money['H']+=1,  count (currency - 50);
+    }
+    else if (currency - 25 >= 0) {
+        return money['Q']+=1,    count (currency - 25);
+    }
+    else if (currency - 10 >= 0) {
+         return money['D']+=1, count (currency - 10);
+    }
+    else if (currency - 5 >= 0) {
+        return money['N']+=1,  count (currency - 5);
+    }
+    else if (currency - 1 >= 0) {
+        return money['P']+=1,  count (currency - 1);
+    }
+}
+
+};
+
